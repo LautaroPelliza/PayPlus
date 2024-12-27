aeca68194987aaac6ad0151fec10946d9626d65a
Lautaro Pelliza <lautypelliza1@gmail.com>
2024-12-26 22:55:48 -0300

    first commit

---
+++ PayPlus/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>PayPlus</h1>
+      <img src="/vite.svg" className="logo" alt="Vite logo" width={100} height={100}/>
+    </>
+  )
+}
+
+export default App
